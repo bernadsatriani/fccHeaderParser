d3e3bb2b57b893bc08ed3d31859bf62a215bf096
Tim <t.grossmann1994@web.de>
2016-09-11 12:27:08 +0000

    improved the system endpoint

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const useragent = require('express-useragent')
 const si = require('systeminformation');
 
 const app = express();
+app.use(useragent.express());
 
 app.set('port', (process.env.PORT ||3000));
 
@@ -9,21 +11,19 @@ app.get('/', (req, res) => {
     const output = {};
     
     output.ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-    output.language = req.headers["accept-language"];
+    output.language = req.headers['accept-language'].split(',')[0];
+    output.system = {};
     
     si.system((data) => {
-        output.system = {};
-        output.system.info = `${data.manufacturer}, ${data.model} ${data.version}`;
+        const systemInfo = req.useragent;
+        
+        output.system.info = `${systemInfo.platform} ${systemInfo.os}, ${systemInfo.browser}`;
     
         si.cpu((data) => {
             output.system.cpu = `${data.manufacturer} ${data.brand}, ${data.speed + 'GHz'} - ${data.cores + ' Cores'}`
             
-            si.osInfo((data) => {
-                output.system.os = `${data.platform}, ${data.logofile} ${data.release}`;
-                
-                res.send(output);
-                res.end();
-            });
+            res.send(output);
+            res.end();
         });
     });
 });
